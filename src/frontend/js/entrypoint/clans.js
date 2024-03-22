a8df954476e1d1e16400842f752ceb96514d737d
beckpaul <beckrpaul@gmail.com>
2024-03-22 15:40:42 -0400

    refactor to clans entrypoint

--- src/frontend/js/entrypoint/clans.js
+++ src/frontend/js/entrypoint/clans.js
@@ -2,42 +2,30 @@ import { DataTable } from 'simple-datatables'
 import axios from 'axios'
 import 'simple-datatables/dist/style.css'
 
-axios
-    .get('/data/clans.json')
-    .then((response) => {
-        if (
-            response.status !== 200 ||
-            !response.data ||
-            !response.data.length
-        ) {
-            console.error('request clans failed')
+async function getClans() {
+    const response = await axios.get('/data/clans.json')
+    if (response.status !== 200 || !response.data || !response.data.length) {
+        throw new Error('Could not retrieve clans')
+    }
 
-            return
+    return response.data
+}
+
+getClans().then((res) => {
+    const dataTable = new DataTable('#clan-table', {
+        perPageSelect: null,
+        data: {
+            headings: ['TAG', 'NAME', 'LEADER', 'POPULATION'],
+            data: res.map((item) => {
+                return [item.tag, item.name, item.leaderName, item.population]
+            }),
+        },
+    })
+
+    dataTable.on('datatable.selectrow', (rowIndex, event) => {
+        if (typeof rowIndex === 'number') {
+            event.preventDefault()
+            window.location.href = '/clans/view/' + res[rowIndex].id
         }
-
-        const clans = response.data
-        const datatable = new DataTable('#clan-table', {
-            perPageSelect: null,
-            data: {
-                headings: ['TAG', 'NAME', 'LEADER', 'POPULATION'],
-                data: clans.map((item) => {
-                    return [
-                        item.tag,
-                        item.name,
-                        item.leaderName,
-                        item.population,
-                    ]
-                }),
-            },
-        })
-
-        datatable.on('datatable.selectrow', (rowIndex, event) => {
-            if (typeof rowIndex === 'number') {
-                event.preventDefault()
-                window.location.href = '/clans/view/' + clans[rowIndex].id
-            }
-        })
     })
-    .catch((err) => {
-        console.log(err, 'loading clans failed')
-    })
+})
