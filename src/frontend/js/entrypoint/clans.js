27e0cb00f1f833c8e10cc6648bb011a3b8a4eb7f
beckpaul <beckrpaul@gmail.com>
2024-04-08 17:31:20 -0400

    undo some "refactoring" and add append a generic error message if clans fail to load

--- src/frontend/js/entrypoint/clans.js
+++ src/frontend/js/entrypoint/clans.js
@@ -2,30 +2,38 @@ import { DataTable } from 'simple-datatables'
 import axios from 'axios'
 import 'simple-datatables/dist/style.css'
 
-async function getClans() {
-    const response = await axios.get('/data/clans.json')
-    if (response.status !== 200 || !response.data || !response.data.length) {
-        throw new Error('Could not retrieve clans')
-    }
+axios
+    .get('/data/clans.json')
+    .then((response) => {
+        const dataTable = new DataTable('#clan-table', {
+            perPageSelect: null,
+            data: {
+                headings: ['TAG', 'NAME', 'LEADER', 'POPULATION'],
+                data: response.data.map((item) => {
+                    return [
+                        item.tag,
+                        item.name,
+                        item.leaderName,
+                        item.population,
+                    ]
+                }),
+            },
+        })
 
-    return response.data
-}
-
-getClans().then((res) => {
-    const dataTable = new DataTable('#clan-table', {
-        perPageSelect: null,
-        data: {
-            headings: ['TAG', 'NAME', 'LEADER', 'POPULATION'],
-            data: res.map((item) => {
-                return [item.tag, item.name, item.leaderName, item.population]
-            }),
-        },
+        dataTable.on('datatable.selectrow', (rowIndex, event) => {
+            if (typeof rowIndex === 'number') {
+                event.preventDefault()
+                window.location.href =
+                    '/clans/view/' + response.data[rowIndex].id
+            }
+        })
     })
+    .catch((error) => {
+        const header = document.querySelector('h1')
+        const errorElement = document.createElement('h2')
+        errorElement.textContent =
+            'Failed to load clans. Please try again later'
 
-    dataTable.on('datatable.selectrow', (rowIndex, event) => {
-        if (typeof rowIndex === 'number') {
-            event.preventDefault()
-            window.location.href = '/clans/view/' + res[rowIndex].id
-        }
+        console.error(error)
+        header.insertAdjacentElement('afterend', errorElement)
     })
-})
